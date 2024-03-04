cbd582a75d82b5293c8f534197f02d299b035538
Trevor Howard <trevorhoward2@gmail.com>
2024-03-03 22:50:19 -0700

    update nextConfig basePath and assetPrefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath:
-    process.env.NODE_ENV === "development" ? "" : "/fatality-discord-bot",
-  assetPrefix:
-    process.env.NODE_ENV === "development" ? "" : "/fatality-discord-bot/",
+  basePath: process.env.NODE_ENV === "development" ? "" : "/portfolio",
+  assetPrefix: process.env.NODE_ENV === "development" ? "" : "/portfolio/",
   output: process.env.NODE_ENV === "development" ? undefined : "export",
   reactStrictMode: true,
   compiler: {
