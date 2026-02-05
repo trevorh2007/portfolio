90ad345d1798b68f32a9ac584fa35f41c548c6ad
Trevor Howard <trevorhoward2@gmail.com>
2026-02-05 09:29:50 -0700

    Update next.config assetPrefix path to correctly load assets on github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: process.env.NODE_ENV === "development" ? "" : "/portfolio",
-  assetPrefix: process.env.NODE_ENV === "development" ? "" : "/portfolio/",
+  assetPrefix: process.env.NODE_ENV === "development" ? "" : "/portfolio",
   output: process.env.NODE_ENV === "development" ? undefined : "export",
   reactStrictMode: true,
   outputFileTracingRoot: process.cwd(),
